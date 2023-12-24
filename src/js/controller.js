e789aaf3a226de222fb493cb28608ae7f4dbb1b0
Samir Mishra <samirmishra.a1@gmail.com>
2023-12-25 03:16:00 +0530

    feat: finish implementing search results pagination.
    Complete pagination functionality by adding pagination view.

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // Poly-filling everything else
 import 'core-js/actual';
@@ -44,16 +45,27 @@ const controlSearchResults = async function() {
         // 2) Load search results
         await model.loadSearchResults(query);
 
-        // 3) Render results
-        // resultsView.render(model.state.search.results);
+        //  Render results
         resultsView.render(model.getSearchResultsPage(1));
+
+        // 4) Render initial pagination buttons
+        paginationView.render(model.state.search);
     } catch (error) {
         console.error(error);
     }
 };
 
+const controlPagination = function(goToPage) {
+    // 1) Render NEW results
+    resultsView.render(model.getSearchResultsPage(goToPage));
+
+    //  Render NEW initial pagination buttons
+    paginationView.render(model.state.search);
+};
+
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
     searchView.addHandlerSearch(controlSearchResults);
+    paginationView.addHandlerClick(controlPagination);
 };
 init();
