7fcb6825d73b302c4d8bdd7952e14a9232ac6d70
Samir Mishra <samirmishra.a1@gmail.com>
2024-01-13 23:26:13 +0530

    refactor: pagination view in recipes search
    Refactor code in pagination view of recipes section on the left side.

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -14,49 +14,47 @@ class PaginationView extends View {
         })
     }
 
+    _getPrevButtonMarkup(pageNo) {
+        return `
+        <button data-goto="${pageNo}" class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${pageNo}</span>
+        </button>
+        `;
+    }
+
+    _getNextButtonMarkup(pageNo) {
+        return `
+        <button data-goto="${pageNo}" class="btn--inline pagination__btn--next">
+            <span>Page ${pageNo}</span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+        </button>
+        `;
+    }
+
     _generateMarkup() {
         const currentPage = this._data.page;
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
 
         // Page 1, and there are other pages
         if (currentPage === 1 && numPages > 1) {
-            return `
-            <button data-goto="${currentPage + 1}" class="btn--inline pagination__btn--next">
-                <span>Page ${currentPage + 1}</span>
-                <svg class="search__icon">
-                    <use href="${icons}#icon-arrow-right"></use>
-                </svg>
-            </button>
-            `;
+            return this._getNextButtonMarkup(currentPage + 1);
         }
 
         // Last page
         if (currentPage === numPages && numPages > 1) {
-            return `
-            <button data-goto="${currentPage - 1}" class="btn--inline pagination__btn--prev">
-                <svg class="search__icon">
-                    <use href="${icons}#icon-arrow-left"></use>
-                </svg>
-                <span>Page ${currentPage - 1}</span>
-            </button>
-            `
+            return this._getPrevButtonMarkup(currentPage - 1);
         }
 
         // Other page
         if (currentPage < numPages) {
             return `
-            <button data-goto="${currentPage - 1}" class="btn--inline pagination__btn--prev">
-                <svg class="search__icon">
-                    <use href="${icons}#icon-arrow-left"></use>
-                </svg>
-                <span>Page ${currentPage - 1}</span>
-            </button>
-            <button data-goto="${currentPage + 1}" class="btn--inline pagination__btn--next">
-                <span>Page ${currentPage + 1}</span>
-                <svg class="search__icon">
-                    <use href="${icons}#icon-arrow-right"></use>
-                </svg>
-            </button>
+            ${this._getPrevButtonMarkup(currentPage - 1)}
+            ${this._getNextButtonMarkup(currentPage + 1)}
             `;
         }
 
